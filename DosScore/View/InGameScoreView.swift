813f67a5044920f832cf3dd25606a6cfdfaf565f
Seravea <romainpoyard@gmail.com>
2024-01-03 13:18:24 +0100

    Adding Modal on the Game view to show the score in Game Creating the View for the Modal Adding 2 buttons on the GameView, First for quit the game playing and the second to show the ScoreModal

---
+++ DosScore/View/InGameScoreView.swift
@@ -0,0 +1,18 @@
+//
+//  InGameScoreView.swift
+//  DosScore
+//
+//  Created by Romain Poyard on 03/01/2024.
+//
+
+import SwiftUI
+
+struct InGameScoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InGameScoreView()
+}
